1927ef92b5aa752419747747f6342c3358ceec8d
EyogLi <138576394+EyogLi@users.noreply.github.com>
2026-07-07 10:46:59 +0800

    Add Color+Extensions.swift

---
+++ kuCar/Core/Extensions/Color+Extensions.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+extension Color {
+
+    /// Create Color from hex string like "#FF5733" or "FF5733".
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.removeFirst()
+        }
+
+        guard hexSanitized.count == 6,
+              let value = UInt(hexSanitized, radix: 16) else {
+            return nil
+        }
+
+        let r = Double((value >> 16) & 0xFF) / 255.0
+        let g = Double((value >> 8) & 0xFF) / 255.0
+        let b = Double(value & 0xFF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
+    }
+
+    /// Hex string representation (e.g., "#FF5733").
+    var hexString: String {
+        let uiColor = UIColor(self)
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        uiColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        return String(format: "#%02X%02X%02X",
+                      Int(r * 255), Int(g * 255), Int(b * 255))
+    }
+}
