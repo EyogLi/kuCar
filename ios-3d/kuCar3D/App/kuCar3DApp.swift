94f69c5bc12ef6e0810d52fa31f1e08b30fc2ee6
EyogLi <138576394+EyogLi@users.noreply.github.com>
2026-07-07 22:35:38 +0800

    feat: add/update ios-3d/kuCar3D/App/kuCar3DApp.swift

---
+++ ios-3d/kuCar3D/App/kuCar3DApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct kuCar3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+                .statusBar(hidden: true)
+        }
+    }
+}
